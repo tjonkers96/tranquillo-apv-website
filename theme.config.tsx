4b1eba1dd6a52b18db0264bc253863a5edcf57b5
Michal <hello@michalk.nl>
2023-11-29 19:11:00 +0100

    initial

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Tranquillo Roleplay - APV</span>,
+  project: {
+    link: "https://github.com/scraayp/tranquillo-roleplay-apv",
+  },
+  chat: {
+    link: "https://discord.gg/tranquilorp",
+  },
+  docsRepositoryBase: "https://github.com/scraayp/tranquillo-roleplay-apv",
+  footer: {
+    text: "Tranquillo Roleplay - APV",
+  },
+};
+
+export default config;
