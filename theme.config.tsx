93922a8d7be94aa114b518b019d484fc386cdef4
Michal <hello@michalk.nl>
2023-11-30 15:51:42 +0100

    Fix repository link

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,12 +3,12 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>Tranquillo Roleplay - APV</span>,
   project: {
-    link: "https://github.com/scraayp/tranquillo-roleplay-apv",
+    link: "https://github.com/Scraayp/tranquillo-apv-website",
   },
   chat: {
     link: "https://discord.gg/tranquilorp",
   },
-  docsRepositoryBase: "https://github.com/scraayp/tranquillo-roleplay-apv",
+  docsRepositoryBase: "https://github.com/Scraayp/tranquillo-apv-website",
   footer: {
     text: "Tranquillo Roleplay - APV",
   },
